0a4900f55c88805aea17dabc5fde6b39341c8b38
Thomas Pope <tpope0928@gmail.com>
2020-03-29 22:51:28 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -3,3 +3,11 @@ const plus = document.getElementById('plus')
 const minus = document.getElementById('minus')
 const pause = document.getElementById('pause')
 const heart = document.getElementById('heart')
+
+function start() {
+  setInterval(increase, 1000);
+}
+
+function increase() {
+  clock.innerText++
+}

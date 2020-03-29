9bae55877b09e7340ca556c2ecc616892a3c6222
Thomas Pope <tpope0928@gmail.com>
2020-03-29 22:50:03 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -3,4 +3,3 @@ const plus = document.getElementById('plus')
 const minus = document.getElementById('minus')
 const pause = document.getElementById('pause')
 const heart = document.getElementById('heart')
-

330dfbf2518632f1732db2ea885580e4e6cb2e75
Thomas Pope <tpope0928@gmail.com>
2020-03-29 22:48:09 +0000

    Automatically backed up by Learn

---
+++ js/challenge.js
@@ -0,0 +1,5 @@
+const clock = document.getElementById('counter')
+const plus = document.getElementById('plus')
+const minus = document.getElementById('minus')
+const pause = document.getElementById('pause')
+const heart = document.getElementById('heart')
